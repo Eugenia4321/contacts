a467526ef683f668f0564e527fa7eaa7ad25989d
Eugenia4321 <113255505+Eugenia4321@users.noreply.github.com>
2022-12-19 15:03:49 +0500

    Update find.py

--- find.py
+++ find.py
@@ -47,4 +47,22 @@ def search_contact_by_phone_num():
         print('У вас в книги нет абонента с таким номером')
 
 # здесь еще нужно ссылки на проверки.  
+def search_contact_return_number_line(data_contact='иванов'):
+    """
+    Функция осуществляет поиск контакта по ФИО или по номеру телефона и возвращает номер строки в файле 
+    Returns:
+    (int)line_number
+    """
+    count = 0
+    line_number=0
+    with open('data.csv', 'r', encoding='utf-8') as file:
+        for line in file:            
+            if data_contact in line:                
+                #print(line)
+                count = line_number
+                return count
+            line_number+=1    
+    if count == 0:
+        return -1
+    return count
 
